3613091ffd92117e6f6c743f735b4647f40c4afb
Srishti556 <147383074+Srishti556@users.noreply.github.com>
2023-11-22 19:24:49 +0530

    Update duckDuckGo.js

--- tests/duckDuckGo.js
+++ tests/duckDuckGo.js
@@ -1,11 +1,20 @@
-describe('duckduckgo example', function() {
-  it('Search Nightwatch.js and check results', function(browser) {
+describe('duckduckgo example', function () {
+  it('Search Nightwatch.js and check results', function (browser) {
     browser
       .navigateTo('https://duckduckgo.com')
       .waitForElementVisible('#search_form_input_homepage')
       .sendKeys('#search_form_input_homepage', ['Nightwatch.js'])
       .click('#search_button_homepage')
       .assert.visible('.results--main')
-      .assert.textContains('.results--main', 'Nightwatch.js');
-  }); 
+      .assert.textContains('.results--main', 'Nightwatch.js')
+
+      // Retrieve and print logs
+      .getLog('msedgedriver', function (result) {
+        result.forEach(entry => {
+          console.log(entry.message);
+        });
+      })
+
+      .end();
+  });
 });
