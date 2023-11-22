02da303515097506671c9ed7846960c6c327a45f
Srishti556 <147383074+Srishti556@users.noreply.github.com>
2023-11-22 17:13:10 +0530

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -59,7 +59,7 @@ module.exports = {
         start_process: true,
         // Download msedgedriver from https://docs.microsoft.com/en-us/microsoft-edge/webdriver-chromium/
         //  and set the location below:
-        server_path: require('path').resolve('./node_modules/.bin/msedgedriver.exe'),
+        server_path: '/home/runner/work/example_nightWatch/example_nightWatch/node_modules/.bin/msedgedriver.exe',
         cli_args: [
           // --verbose
         ]
@@ -82,7 +82,7 @@ module.exports = {
         start_process: true,
         // Download msedgedriver from https://docs.microsoft.com/en-us/microsoft-edge/webdriver-chromium/
         //  and set the location below:
-        server_path: require('path').resolve('./node_modules/.bin/msedgedriver.exe'),
+        server_path: '/home/runner/work/example_nightWatch/example_nightWatch/node_modules/.bin/msedgedriver.exe',
         cli_args: [
           // --verbose
         ]
